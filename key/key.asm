f5ce1c5158a2a5f51c95b222a53fb342a743f7ca
descendency <descendency@outlook.com>
2019-03-06 19:08:04 -0500

    Fixing some spacing (test)

--- key/key.asm
+++ key/key.asm
@@ -46,7 +46,7 @@ getloop:									;
 		mov 	BX, AX						; Move transition code into bx
 		add 	BX, BX						; double for word data
 		mov 	AX, CS:[trans + BX]			; find new read/write/exit code
-		mov 	DL, AL						; prepare to write a char (DH=char)
+		mov		DL, AL						; prepare to write a char (DH=char)
 		int 	21h							; DOS interrupt issued.
 		jmp 	getloop						; jump to the top of the loop.
 ;------------------------------------------------------------------------------
@@ -55,27 +55,27 @@ getloop:									;
 trans	dw 	022Eh dup (0800h)				; Read a new character, no print required (022Eh)
 		dw 	4C00h							; Period Printed (022Eh), now exit
 		dw 	05D1h dup (0800h)				; Read a new character, no print required (0800h)
-		dw  20h dup (0800h)					; Read a new character, no print required (space)
-		dw  0220h							; print 'space'
+		dw	20h dup (0800h)					; Read a new character, no print required (space)
+		dw	0220h							; print 'space'
 		dw 	0Dh dup (0800h)					; Read a new character, no print required (period)
-		dw  022Eh							; print period
+		dw	022Eh							; print period
 		dw	18 dup (0800h) 					; Read a new character, no print required ('A')
 											; Print:
 		dw 	0241h,0242h,0243h,0244h,0245h	; A-E
-		dw  0246h,0247h,0248h,0249h,024Ah	; F-J
+		dw	0246h,0247h,0248h,0249h,024Ah	; F-J
 		dw 	024Bh,024Ch,024Dh,024Eh,024Fh	; K-O
-		dw  0250h,0251h,0252h,0253h,0254h	; P-T
-		dw  0255h,0256h,0257h,0258h,0259h	; U-Y
-		dw  025Ah							; Z
-		dw 	6 dup (0800h) 				    ; Read a new character, no print required ('a')
+		dw	0250h,0251h,0252h,0253h,0254h	; P-T
+		dw	0255h,0256h,0257h,0258h,0259h	; U-Y
+		dw	025Ah							; Z
+		dw	6 dup (0800h) 				    ; Read a new character, no print required ('a')
 											; Print and Change:
-		dw 	0241h,0242h,0243h,0244h,0245h	; a-e to A-E
-		dw  0246h,0247h,0248h,0249h,024Ah	; f-j to F-J
-		dw 	024Bh,024Ch,024Dh,024Eh,024Fh	; k-o to K-O
-		dw  0250h,0251h,0252h,0253h,0254h	; p-t to P-T
-		dw  0255h,0256h,0257h,0258h,0259h	; u-y to U-Y
-		dw  025Ah							; z to Z
-		dw 	133 dup (0800h) 				; Read a new character, no print required (remaining)
+		dw	0241h,0242h,0243h,0244h,0245h	; a-e to A-E
+		dw	0246h,0247h,0248h,0249h,024Ah	; f-j to F-J
+		dw	024Bh,024Ch,024Dh,024Eh,024Fh	; k-o to K-O
+		dw	0250h,0251h,0252h,0253h,0254h	; p-t to P-T
+		dw	0255h,0256h,0257h,0258h,0259h	; u-y to U-Y
+		dw	025Ah							; z to Z
+		dw	133 dup (0800h) 				; Read a new character, no print required (remaining)
 end 	 									; end of program marker
 ;------------------------------------------------------------------------------
 ; END OF PROGRAM KEY
